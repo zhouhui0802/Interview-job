c86e408b77ee59427548e4b91e1908767718ec83
zhouhui0802 <1741484908@qq.com>
2025-12-25 07:39:06 +0800

    面试准备的代码，一步步执行 （1）mybatis-plus生成代码

---
+++ srb-parent/srb-common/src/main/java/com/zh/common/commonApplication.java
@@ -0,0 +1,11 @@
+package com.zh.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class commonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(commonApplication.class, args);
+    }
+}
---
+++ srb-parent/srb-service-base/src/main/java/com/zh/srb/base/baseApplication.java
@@ -0,0 +1,11 @@
+package com.zh.srb.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class baseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(baseApplication.class, args);
+    }
+}
