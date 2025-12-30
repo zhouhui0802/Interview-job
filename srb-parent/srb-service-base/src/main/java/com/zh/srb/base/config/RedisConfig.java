31a36d14aca330da475289ef4b31eefbfdc1d54e
zhouhui0802 <1741484908@qq.com>
2025-12-30 19:01:58 +0800

    整体项目暂时停止，开始着手布局各种中间件 大项目开始以亲自动手为主

--- srb-parent/srb-service-base/src/main/java/com/zh/srb/base/config/RedisConfig.java
+++ srb-parent/srb-service-base/src/main/java/com/zh/srb/base/config/RedisConfig.java
@@ -10,7 +10,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
 @Configuration
 public class RedisConfig {
 
--- srb-parent/srb-service-core/src/main/java/com/zh/ServiceCoreApp.java
+++ srb-parent/srb-service-core/src/main/java/com/zh/ServiceCoreApp.java
@@ -2,7 +2,6 @@ package com.zh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * Hello world!
  *
@@ -12,7 +11,14 @@ public class ServiceCoreApp
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(ServiceCoreApp.class, args);
-        System.out.println( "Hello World!" );
+        try {
+            SpringApplication.run(ServiceCoreApp.class, args);
+            System.out.println( "Hello World!" );
+        } catch (Exception e) {
+            System.err.println("SpringBoot Application Startup Failed: " + e.getMessage());
+            e.printStackTrace(); // 打印堆栈信息
+            // 可以在这里进行更详细的日志记录或资源释放
+        }
+
     }
 }
